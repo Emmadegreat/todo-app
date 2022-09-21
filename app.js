653b7de669e13edb25f2fbb879e2cc877f9584e3
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2022-09-21 01:10:20 -0700

    added

---
+++ app.js
@@ -0,0 +1,51 @@
+let input = document.getElementById('add-item');
+let form = document.getElementById('myform');
+let btn = document.getElementById('add-list');
+let h1 = document.createElement('h1');
+let tasks = document.querySelector('.tasks');
+
+
+input.addEventListener('keyup', () => {
+    //e.preventDefault();
+    if (input.value.trim() != 0) {
+        btn.classList.add('active');
+    } else {
+        btn.classList.remove('active');
+    }
+})
+
+
+//add task
+btn.addEventListener('click', () => {
+    if (input.value.trim() != 0) {
+        let newItem = document.createElement('div');
+        newItem.classList.add('item');
+        newItem.innerHTML = `
+        <p>${input.value}</p>
+        <div class="item-btn">
+            <button class="cross">&#10004;</button>
+            <button class="del">delete</button>
+        </div>
+        `
+        tasks.appendChild(newItem);
+        input.value = '';
+    } else {
+        h1.textContent = 'please enter a task';
+        tasks.appendChild(h1);
+    }
+})
+
+//delete task
+tasks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('del')) {
+        e.target.parentElement.parentElement.remove();
+    }
+})
+
+//complete task
+tasks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('cross')) {
+        e.target.parentElement.parentElement.classList.toggle('completed');
+    }
+})
+
